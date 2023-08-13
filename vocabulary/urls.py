5fdf5f70981cc00922cb99e6a0e1e0a28142fb5e
hongggs <hongseoda@gmail.com>
2023-08-13 16:42:19 +0900

    Feat: 단어 시험지 단어 리스팅 기능
    저장된 단어 중 랜덤으로 20개의 단어를 json 형식으로 리턴

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import VocaList
+from .views import VocaList, VocaTestList
 
 urlpatterns = [
     path('', VocaList.as_view(), name = 'voca-list'),
+    path('exam/start', VocaTestList.as_view(), name = 'voca-test-list'),
 ]
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -8,17 +8,31 @@ from .models import Vocabulary
 
 class VocaList(APIView):
 
+    # 단어 리스팅
     def get(self, request):
         user_id = request.GET.get('id')
         queryset = Vocabulary.objects.filter(user_id=user_id)
         serializer = vocaSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    # 단어 저장
     def post(self, request):
-        print(request.data)
         voca = vocaSerializer(data=request.data)
-        print(voca)
         if voca.is_valid():
             voca.save()
             return Response(voca.data)
         return Response(voca.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VocaTestList(APIView):
+
+    # 저장된 단어 중 랜덤으로 20개를 가져와 시험 단어 세트 리스팅
+    def get(self, request):
+        user_id = request.GET.get('id')
+        queryset = Vocabulary.objects.filter(user_id=user_id).order_by('?')[:20]
+        serializer = vocaSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+
